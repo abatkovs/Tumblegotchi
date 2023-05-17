47250b324030758f5b24817250061c1053b79245
Dragoons <arnolds.batkovskis@gmail.com>
2023-05-17 17:24:03 +0300

    Intro

--- Assets/_MyAssets/Scripts/Intro.cs
+++ Assets/_MyAssets/Scripts/Intro.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,23 @@ using UnityEngine;
 public class Intro : MonoBehaviour
 {
     [SerializeField] private SoundData introSound;
+    [SerializeField] private GameObject game;
+
+    [SerializeField] private SoundManager soundManager;
+
+    private void Start()
+    {
+        game.SetActive(false);
+    }
 
     private void PlayIntroSound()
     {
-        
+        soundManager.PlaySound(introSound);
     }
 
     private void FinishIntro()
     {
+        game.SetActive(true);
         gameObject.SetActive(false);
     }
 }
