6599a23ea57c8dd27e0313378df9e6bf6847c00d
Dragoons <arnolds.batkovskis@gmail.com>
2023-06-30 12:19:59 +0300

    Improve game dragging

--- Assets/_MyAssets/Scripts/DraggingAround.cs
+++ Assets/_MyAssets/Scripts/DraggingAround.cs
@@ -4,10 +4,15 @@ public class DraggingAround : MonoBehaviour
 {
     [SerializeField] private Transform game;
     [SerializeField] private Button button;
+    [Space] 
+    [SerializeField] private Vector3 draggingOffset;
+    [SerializeField] private float minX = -6.5f, maxX = 8f, minY = -0.5f, maxY = 4.8f;
 
     private bool _isDragged;
     private Camera _camera;
     private Vector3 _updatedPosition;
+    private float _screenWidth;
+    private float _screenHeight;
 
     private void Start()
     {
@@ -19,9 +24,33 @@ public class DraggingAround : MonoBehaviour
 
     private void Update()
     {
+        DragGameAround();
+    }
+
+    private void ConfineDraggingArea()
+    {
+        var mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        
+    }
+
+    private void DragGameAround()
+    {
         if (_isDragged)
         {
-            _updatedPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            var mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log($"{mousePos.x > minX && mousePos.x < maxX}");
+            if (mousePos.x > minX && mousePos.x < maxX)
+            {
+                _updatedPosition.x = mousePos.x;
+                _updatedPosition.x -= draggingOffset.x;
+            }
+            
+            if (mousePos.y > minY && mousePos.y < maxY)
+            {
+                _updatedPosition.y = mousePos.y;
+                _updatedPosition.y -= draggingOffset.y;
+            }
+
             _updatedPosition.z = 0;
             game.position = _updatedPosition;
         }
@@ -29,12 +58,17 @@ public class DraggingAround : MonoBehaviour
 
     private void ButtonOnOnButtonDown()
     {
+        var gamePos = game.position;
+        var mouseWorldPos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        draggingOffset = mouseWorldPos - gamePos;
+        //Debug.Log($"g: {gamePos} w: {mouseWorldPos} d: {draggingOffset}");
         ToggleDragging(true);
     }
 
     private void ButtonOnOnButtonReleased()
     {
         ToggleDragging(false);
+        Debug.Log(_camera.ScreenToWorldPoint(Input.mousePosition));
     }
     
     private void ToggleDragging(bool value)
